ae41b1e3339d0ef6cdcc3edb8d6c4b8a4110b886
Oliver <omiguezalonso@gmail.com>
2024-10-10 14:29:21 +0200

    eje_4

---
+++ Boletin_2_Extra/src/main/java/org/example/eje_4.java
@@ -0,0 +1,25 @@
+package org.example;
+import  java.util.Scanner;
+public class eje_4 {
+    public  static  void  main(String[]args){
+        int Dinero;
+        Scanner a = new Scanner(System.in);
+        System.out.println("cual es tu dinero: ");
+        Dinero = a.nextInt();
+
+
+        int num1 = Dinero/100;
+        int Billete1 = Dinero - (num1 *100);
+        int num2 = Billete1/20;
+        int Billete2 = Billete1  - (num2 *20);
+        int num3 = Billete2/5;
+        int Billete3 = Billete2 - (num3*5);
+        int num4 = Billete3/1;
+
+        int Billet4 = Billete3 -(num4*1);
+
+
+
+        System.out.println("Billetes 100 :"+ num1+ ", Billetes 20: "+ num2 + ", Billetes 5:"+ num3+ ", Monedas: "+ num4);
+    }
+    }
