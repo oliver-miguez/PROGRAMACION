1b7cb94140112b3c2df832ca69d8ee4a00cfbf17
XogosAquiDEV <olimial03@gmail.com>
2024-10-26 13:12:03 +0200

    eje_5_acabar de hacer

---
+++ Boletin_5/src/eje_5.java
@@ -0,0 +1,26 @@
+/**
+ * Codifica un programa que lea o soldo de cada un dos traballadores dunha empresa
+ * e obteña o número deles que ganan entre 1000 e 1750 €, ámbolos dous incluídos e,
+ * a porcentaxe de traballadores que ganan menos de 1000 €. Tendo en conta que non se
+ * coñece con antelación o numero de traballadores da empresa e non se admiten os soldos
+ * negativos (utiliza como condición de fin un soldo 0).
+ *
+ * @author Oliver Miguez Alonso
+ * @version 1.0
+ */
+
+import java.util.Scanner;//Para poder utilizar la funcion Scanner
+public class eje_5 {
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);//Permite introducir datos por consola
+
+        System.out.println("Introduce o numero de traballadores");
+        System.out.print("Introduce o valor do teu soldo");//pregunta por pantalla
+        int soldo = sc.nextInt();//almacena el valor introducido por consola
+
+
+
+
+    }//end static void
+}//end class
