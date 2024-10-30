ef20b8633070b2383ebf80ec227eadd0450d66ed
Oliver <omiguezalonso@gmail.com>
2024-10-30 09:02:43 +0100

    eje_2

---
+++ Boletin_2/src/eje_2.java
@@ -0,0 +1,15 @@
+
+/**Calculo area cuadrado
+ *@version 1.1
+ * @author Oliver Miguez Alonso
+ */
+public class eje_2{
+    public static void main(String[] args){
+        //variable
+        int l=3;
+        int area=l*l;
+        //resultado
+        System.out.println("El área de tu cuadrado de lado 3 es: "+area);
+
+    }// end static void
+}//end class
