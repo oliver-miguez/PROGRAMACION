36d62cfbcbac8897a25c951491ea2d914f57a2d1
Oliver <omiguezalonso@gmail.com>
2024-11-04 13:03:16 +0100

    eje_5

--- Boletin_6/src/eje_5.java
+++ Boletin_6/src/eje_5.java
@@ -1,33 +1,48 @@
 import java.util.Scanner; //permite introducir datos por consola
+
 /**
  * Deseña unha función a que se lle pasan as horas e minutos de dous intantes de tempo co seguinte prototipo:
  * static int diferenciaMin (int hora1, int minuto1, int hora2, int minuto2)
  * A función devoltará a diferencia en minutos que existen entre os dous instantes de tempo.
+ *
  * @author Oliver Miguez Alonso
  * @version 1.0
  */
 public class eje_5 {
     /**
      * Método principal del progama
+     *
      * @param args no usamos
      */
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in); //Alamacena datos introducidos por consola
-        System.out.println("Introduce la hora 1: ");
-        int hora1 = sc.nextInt(); //Almacena el dato de hora 1 introduucido por consola
-        System.out.println("Introduce el minuto 1 : ");
-        int minuto1 = sc.nextInt(); //Almacena el dato de minuto 1 introduucido por consola
-        System.out.println("Introduce la hora 2 : ");
-        int hora2 = sc.nextInt(); //Almacena el dato de hora 2 introduucido por consola
-        System.out.println("Introduce el minuto 2: ");
-        int minuto2 = sc.nextInt(); //Almacena el dato de minuto 2 introduucido por consola
+    public static void main(String[] args) {
+        int hora1 = 1;
+        int hora2 = 2;
+        int minuto1 = 20;
+        int minuto2 = 30;
 
-        System.out.println(diferenciaMin());
+        //Integra la función diferencia para mostrar el resultado de la diferencia del tiempo
+
+        int diferencia = diferenciaMin(hora1, minuto1, hora2, minuto2);//crea la funcion diferencia para poder mostrar el resultado
+        System.out.println("La diferenica es " + diferencia);
+
+
     } //end static void main
 
-    static int diferenciaMin (int hora1, int minuto1, int hora2, int minuto2){
-       int total_minutos1 = hora1*60 + minuto1;//minutos total 1
-       int total_minuto2 = hor2*60 + minuto2;//minutos total 2
-       return total_minutos1 - total_minuto2; // Devuelve el resultado del total 1 y total 2
+
+    /**
+     * Funcion que calcula la diferencia de las horas
+     *
+     * @param hora1   valor de la hora 1
+     * @param minuto1 valor del minuto 1
+     * @param hora2   valor de la hora 2
+     * @param minuto2 valor del minuto 1
+     * @return devuelve la diferencia
+     */
+    static int diferenciaMin(int hora1, int minuto1, int hora2, int minuto2) {
+
+        int total_minutos1 = hora1 * 60 + minuto1;//minutos total 1
+        int total_minuto2 = hora2 * 60 + minuto2;//minutos total 2
+        int oper = total_minuto2 - total_minutos1; // calculo de la diferencia
+        return oper; // Devuelve el resultado de la diferencia
     }//end static int
 } //end class
