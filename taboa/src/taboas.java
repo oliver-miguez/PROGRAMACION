64b6a82bda6683c788c74e1d9c6c48fd39e8ab0e
Oliver <omiguezalonso@gmail.com>
2024-11-06 10:41:02 +0100

    Uso de las tablas

--- taboa/src/taboas.java
+++ taboa/src/taboas.java
@@ -12,6 +12,16 @@ public class taboas {
         int[]edades = {1,2,3,4,5};//Tabla que tiene valores ya integrados, reservados en la memoria, 5 valores almacenados
         String[]nomes;//Indica que existira una tabla de nomes pero que aun no esta formada, ni tiene un espacio reservado en memoria
         nomes = new String[5];//Indica el tamaño de la tabla, reserva ya un espacion en la memoria, almacena 5 strings
+        nomes[0]="Oliver"; //intrdcue el valor dentro de la tabla en la posicion 0
+        nomes[1]="Miguez";//introduce el valor dentro de la tabla en la posicion 1
+        /*podemos introducir valores hasta el 4, del 0 al 4 */
+
+
+
+         /*
+         Sin embargos con tablas ya hechas como la tabla de edades , no hace falta definir entre corchetes la cantidad de entidades que
+         usara , debido a que el IDE ya detecta y cuenta cuantos valores tiene ya introducidos
+          */
 
         /* o en caso en el caso del int */
         edades = new int[5];//cambia de nuevo la estructura de la tabla edades
