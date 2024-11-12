16c3aa6168b317a71d72f960a62035a3fc1a5a5d
Oliver <omiguezalonso@gmail.com>
2024-11-12 13:50:34 +0100

    eje_1 arreglos

--- Boletin_6/src/eje_1.java
+++ Boletin_6/src/eje_1.java
@@ -31,6 +31,16 @@ public class eje_1 {
         System.out.println("Numero 2: ");//Pregunta el valor del numero 2
         num2 = sc.nextInt();//almacena los datos introducidos
 
+        /*
+        Si en el caso de que en el for el numero 1 sea el mayor de los dos numeros , cambia su valor con el numero 2
+         */
+        if(num1>num2){
+            int axuliar = num1; //para modificar el numero uno sin perder su valor inicial
+            num1 = num2; // Transforma el valor de num1 al valor mas pequeño que es num 2
+            num2 = axuliar; //le damos el valor de numero uno inicial al numero dos para transformalo en el numero mayor
+
+        }//end if
+
         //Muestra los numeros que estan entre el numero 1 y el numero 2
         for (int i = num1 + 1; i < num2; i++) {
             System.out.println(i);
