d10fe6fcaa3ea4786952e98f84f2731bbc5ce896
Oliver <omiguezalonso@gmail.com>
2024-11-25 13:32:49 +0100

    Practica_1_con_array

--- Practica_con_array/src/Practica_1_con_array.java
+++ Practica_con_array/src/Practica_1_con_array.java
@@ -1,38 +1,55 @@
 /**
- * Codificar unha función que faga unha copia dunha matríz pasada como parámetro e devolte a copia do matríz.
- * devuelve una copia de un array
+ *Codificar unha función que faga unha copia dunha matríz pasada como parámetro e devolte a copia do matríz.
  */
 public class Practica_1_con_array {
     /**
      * Metodo principal del programa
-     *
      * @param args no usamos
      */
     public static void main(String[] args) {
+
         int[] lista = new int[10]; //lista de 10 numeros que clonará el programa
+
         //genera 10 numeros ramdon y los añade  a la lista
         for (int i = 0; i < lista.length; i++) {
             lista[i] = (int) (Math.random() * 10);
-            System.out.println(lista[i]);//muestra la lista
         }//end for
+
         System.out.println("_____________________________________");
+
         clonar(lista);
+
+        //comprueba para saber si son una misma lista o si son distintas
+        if(lista == clonar(lista)){
+            System.out.println("Las listas son la misma");
+        }//end if
+        else{
+            System.out.println("Son listas distintas");
+        }//end else
+
+
     }//end main
 
     /**
      * Clona la lista
      *
-     * @param lista introduce los valores del array lista
-     * @return lista clonada
+     * @return retorna la nueva lista clonar
      */
     static int[] clonar(int[] lista) {
-        System.out.println("lista clonada:");
-        int[] clonar = lista;
+        //nuevo array, que copiara el array
+        int[] clonar;
+        clonar = new int[lista.length];//coge el mismo tamaño que el array lista
+
+        //entra en la lista clonar , para igualar todos los valores con los del array lista
         for (int j = 0; j < clonar.length; j++) {
+            clonar[j]=lista[j];//da a la lista clonar , los valores del array lista
             System.out.println(clonar[j]);
+
         }//end for
-        System.out.println("_________________________________");
+        System.out.println("_______________________________________");
         return clonar;
+
+
     }//end clonar
 
 }//end class
