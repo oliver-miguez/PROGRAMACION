2821ba19bd485c27c42cdb14e47e9c2fd2efdb54
Oliver <omiguezalonso@gmail.com>
2024-12-18 10:38:54 +0100

    eje1

---
+++ Boletin 8/src/eje1.java
@@ -0,0 +1,9 @@
+/**
+ * Mostra a lonxitude do texto: “ Isto é Java!”
+ */
+public class eje1 {
+    public static void main(String[] args) {
+        String texto = "Isto é java";
+        System.out.println(texto.length());
+    }//end main
+}//end class
