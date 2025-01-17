b3fbb1bcc7ba3e9a37321a295601862a19d1e15f
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-01-17 09:09:33 +0100

    eje 12 conCaracteres

---
+++ Boletin 8.2/src/eje12.java
@@ -0,0 +1,21 @@
+/**
+ * Crear a función que permíta realizar un analisis simple de texto.
+ * O analizador ten  a función de contar palabras, caracteres e encontrar a palabra mais longa. Mostrar os resultados por pantalla.
+ * @author Oliver Miguez Alonso
+ */
+public class eje12 {
+    /**
+     * Metodo principal del programa
+     * @param args no usamos
+     */
+    public static void main(String[] args) {
+        String palabra1 = "Oye como va mi ritmo";
+        String palabra2 = "Jamón serrano";
+    }//end main
+
+    public static String contarCaracteres(String palabra1 , String palabra2){
+
+    }//end contarCaracteres
+
+
+}//end class
