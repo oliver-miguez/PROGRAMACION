853cc5987198946fa58a96022e64d2d318491b42
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-01-20 14:28:04 +0100

    aprendiendo clases

---
+++ introduccion a la creacion de objetos/src/Libros.java
@@ -0,0 +1,23 @@
+/**
+ * Clase que representa a un libro
+ * @author Oliver Miguez Alonso
+ */
+public class Libros {
+    //propiedades de clase libro
+    //Representa el formato de clase que usaremos(public, private) el tipo (String, int) y el nombre de la clase
+    public String titulo;
+    public String autor;
+    private int numPaginas;
+    private double precio;
+    private int cantidad;
+
+
+    public boolean venderLibro(int cantidadVenta){
+        //todo - por hacer
+    }//end vendeLibro
+
+    public int registrarEntradaLibro(int cantidadEntrada){
+        //todo - por hacer
+    }//end comprarLibro
+
+}//end class Libros
