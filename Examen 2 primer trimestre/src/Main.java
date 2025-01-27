2690afc41a99ce935f03ab0b0edebb75b28af619
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-01-27 13:41:45 +0100

    eje 1

---
+++ Examen 2 primer trimestre/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Oliver Miguez Alonso
+ */
+public class Main {
+    /**
+     * Metodo principal del programa
+     * @param args no usamos
+     */
+    public static void main(String[] args) {
+
+    }//end main
+}//end class
