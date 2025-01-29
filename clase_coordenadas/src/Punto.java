a902ab1daa9418f3075e5d5ab9b97df0fe0b2f24
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-01-29 10:04:21 +0100

    MÁS PRACTICAS CON CLASES

---
+++ clase_coordenadas/src/Punto.java
@@ -0,0 +1,24 @@
+public class Punto {
+    public double x;
+    public double y;
+
+    public Punto(){
+
+    }
+
+    public Punto(double x){
+
+    }
+    public Punto(int x) {
+
+    }
+
+    public Punto(double x, double y){
+
+    }
+
+    public Punto(int x, int y){
+
+    }
+
+}
