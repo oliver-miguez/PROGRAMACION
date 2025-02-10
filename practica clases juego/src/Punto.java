2a146431b8fb32612b4b8c9641451f2daa969270
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-10 13:59:50 +0100

    arreglos en clase Punto

--- practica clases juego/src/Punto.java
+++ practica clases juego/src/Punto.java
@@ -1,41 +1,45 @@
 public class Punto {
-    public double x;
-    public double y;
+    private double x;
+    private double y;
 
     public Punto(){
         x = 0;
         y = 0;
-    }
+    }//endPunto
     public Punto(double w){
         x = w;
         y = w;
-    }
+    }//endPunto
     public Punto(int w){
         x = w;
         y = w;
-    }
+    }//endPunto
     public Punto(double x, double y){
         this.x = x;
         this.y = y;
-    }
+    }//endPunto
+    public Punto(int x, int y){
+        this.x = x;
+        this.y = y;
+    }//endPunto
 
 
     //SETERS Y GETERS
 
     public void setX(double x){
-        if(x <= 0 ) this.x = x;
+        if(x >= 0 ) this.x = x;
         else x = 0;
-    }//end X
+    }//end setX
     public double getX (){
         return x;
-    }
+    }//end getX
 
     public void setY(double y){
-        if(y <= 0 ) this.y = y;
+        if(y >= 0 ) this.y = y;
         else y = 0;
-    }//end X
+    }//end setY
     public double getY (){
         return y;
-    }
+    }//end getY
 
 }//end class
