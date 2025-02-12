e5bc34503fa681cb24245713abb5cc7d9eceda9e
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-12 09:12:22 +0100

    clase data basica

---
+++ clase_data/src/Data.java
@@ -0,0 +1,58 @@
+/**
+ * Definición de una clase data
+ * @author Oliver Miguez Alonso
+ */
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+    private String data;
+
+    //constructor de la clase
+    public Data(){
+        setDia(dia);
+        setMes(mes);
+        setAno(ano);
+        setData(data);
+    }//end Data
+
+    /*
+    SETTERS Y GETTERS
+     */
+    public int getDia() {
+        return dia;
+    }//end getDia
+
+    public void setDia(int dia) {
+        this.dia = dia + 1; //aumenta en uno el dia
+    }//end setDia
+
+    public int getMes() {
+        return mes;
+    }//end getMes
+
+    public void setMes(int mes) {
+        this.mes = mes + 1;
+    }//end setMes
+
+    public int getAno() {
+        return ano;
+    }//end getAno
+
+    public void setAno(int ano) {
+        this.ano = ano + 1;
+    }//end setAno
+
+
+    public String getData() {
+        return data;
+    }//end getData
+
+    public void setData(String data) {
+        this.data = data;
+    }//end setData
+
+    /*
+    END SETTERS Y GETTERS
+     */
+}//end class
---
+++ clase_data/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Clase principal del programa de "data"
+ * @author Oliver Miguez Alonso
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
