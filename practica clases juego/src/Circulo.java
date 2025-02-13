e0ab52f984951ce2148f218af63e038ed2bd2651
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-13 13:58:30 +0100

    clase circulo conectada a la clase punto

---
+++ practica clases juego/src/Circulo.java
@@ -0,0 +1,12 @@
+/**
+ * Distintas operaciones que se pueden realizar con un círculo
+ * @author Oliver Miguez Alonso
+ */
+public class Circulo extends Punto{ //crea una clase circulo que hereda todo de la clase punto
+    //declaración de propiedades
+
+    //constructor
+    public Circulo (){
+
+    }//end constructor
+}//end Circulo
