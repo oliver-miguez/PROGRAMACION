1f590c7020c3da2f1ff729a17a023566b55ee493
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-13 13:18:00 +0100

    comentarios clase Punto

--- practica clases juego/src/Punto.java
+++ practica clases juego/src/Punto.java
@@ -61,6 +61,9 @@ public class Punto {
         return y;
     }//end getY
 
+
+    //METODOS DE OBJET (equals)
+
     //reescribimos la variable equals que afecta en la main , para comparar los dos puntos
     public boolean equals(Punto outroPunto){
         if((x == outroPunto.x) && (y == outroPunto.y) ) return true;
