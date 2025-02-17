3e2fcbaee558a2e26f4e8458a02b02690708ded8
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-17 14:51:16 +0100

    Acabar constructor prisma

--- practica clases juego/src/Prisma.java
+++ practica clases juego/src/Prisma.java
@@ -1,7 +1,41 @@
-public class Prisma extends Cilindro{
+public class Prisma {
+
+
+    //Variables
+    private double largo;
+    private double ancho;
+    private double altura;
+
 
     //CONSTRUCTOR
-    public Prisma(double x, double y, double r, double h) {
-        super(x, y, r, h);
-    }//end constructor
+    public Prisma( double l){
+        setLargo(l);
+    }//en Prisma Constructor
+
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        if (largo < 0 ) this.largo = 1;
+        else this.largo = largo;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
 }//end Prisma class
