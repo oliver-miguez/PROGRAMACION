dd14f6fe30784dc31748001bea38ac9fe2c552b6
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-26 09:23:51 +0100

    Definir clase Punto y constructor  getters y setters

--- CLASES/PracticaConClasesAbstractas/src/Punto_Abstracto.java
+++ CLASES/PracticaConClasesAbstractas/src/Punto_Abstracto.java
@@ -13,11 +13,42 @@
  */
 public class Punto_Abstracto {
     //Atributos del objeto
-
+    private double x;
+    private double y;
 
     //CONSTRUCTOR
     public Punto_Abstracto(){
+        x = 0;
+        y = 0;
+    }//end constructor
+
+    public Punto_Abstracto(double x, double y){
+        setX(x);
+        setY(y);
 
     }//end constructor
+
+    //SETTERS Y GETTERS
+
+    //X
+    public double getX() {
+        return x;
+    }//end getX
+
+    public void setX(double x) {
+        if ( x < 0) this.x = 0;
+        else this.x = x;
+    }//end setX
+
+    //Y
+    public double getY() {
+        return y;
+    }//end getY
+
+    public void setY(double y) {
+        if(x < 0) this.y = 0;
+        else this.y = y;
+    }//end setY
+
 
 }// end class
