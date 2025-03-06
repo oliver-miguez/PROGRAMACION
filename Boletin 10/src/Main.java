70f6b9ce8b2a25672a12f373fa9dfef7f8ba0f15
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-03-06 14:51:24 +0100

    Arreglar errores !!!

--- Boletin 10/src/Main.java
+++ Boletin 10/src/Main.java
@@ -8,6 +8,10 @@ public class Main {
      * @param args No usamos
      */
     public static void main(String[] args) {
+       PlazaGaraxe plaza1 = new   PlazaGaraxe("Garcia Borbón", 12, "Vigo",100, true, 4, 5 , 6);
+        System.out.println(plaza1);
 
+        Vivenda vivenda1 = new Vivenda("Garcia borbon", 2,"Vigo", 1000, true, "segundo", "Terceiro ", 6,plaza1,"Nada");
+        System.out.println(vivenda1);
     }//end static
 }//end main
