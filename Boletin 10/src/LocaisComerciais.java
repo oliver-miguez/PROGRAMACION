d8de2d4b1d63a87542cfe0a08a7dc034d606eb18
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-03-06 13:03:20 +0100

    Añadiendo clases con las que trabajaré

---
+++ Boletin 10/src/LocaisComerciais.java
@@ -0,0 +1,2 @@
+public class LocaisComerciais {
+}
---
+++ Boletin 10/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author Oliver
+ * @version 1.0
+ */
+public class Main {
+    /**
+     * Metodo principal del programa
+     * @param args No usamos
+     */
+    public static void main(String[] args) {
+
+    }//end static
+}//end main
