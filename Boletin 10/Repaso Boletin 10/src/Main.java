5d5c813cd7be0da92f37673a047f9991e75ebd67
XogosAquiDEV <olimial03@gmail.com>
2025-03-12 19:22:06 +0100

    Primer commit (repaso boletin 10)

---
+++ Boletin 10/Repaso Boletin 10/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Repaso de Boletín 10 de clases Abstractas
+ * @author Oliver Miguez Alonso
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }//end main
+}//end class
