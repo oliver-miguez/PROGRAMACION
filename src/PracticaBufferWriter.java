52874f0c1c4a6acb30f8c86942b69131ef693805
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-04-03 14:51:06 +0200

    BufferedWriter

---
+++ src/PracticaBufferWriter.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+/**
+ * Permite introducir texto en un archivo de texto dede consola
+ * @author Oliver Miguez Alonso
+ */
+public class PracticaBufferWriter {
+    /**
+     * Metodo principal del programa
+     * @param args No usamos
+     */
+    public static void main(String[] args) {
+
+        BufferedWriter saida = null;
+
+        try{
+
+        }catch (IOException e){
+            System.out.println("Error en la verificación de la existencia del archivo: " + e.getMessage());
+        }
+
+    }//end main
+}//end class
