15348442f3e41404bd3e0560df4aefe4530cda0b
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-04-23 09:28:50 +0200

    Tipos diferentes de contenedores 2

--- Collection/src/Main.java
+++ Collection/src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         //Contenedor solo para objetos tipo integer
+        //1ºForma
         Contedor<Integer> num = new Contedor<Integer>();
         num.gardar(5);
         System.out.println(num.extraer());
-        Contedor<String > cad = new Contedor<String>();
+        //Contenedor para Strings
+        //2ºForma
+        Contedor<String > cad = new Contedor<>();
         cad.gardar("Ola mundo");
         System.out.println(cad.extraer());
+        //3ºForma
+        Contedor ele = new Contedor();
+        ele.gardar(6);
+        System.out.println(ele.extraer());
 
     }
 }
