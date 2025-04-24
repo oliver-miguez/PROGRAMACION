39f2751da95ceea55f94bd02d1d3bf9bf1094413
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-04-24 14:34:16 +0200

    TratamentoDeClientes: pequeños métodos

--- Collection/Compare/src/TratamentoDeClientes.java
+++ Collection/Compare/src/TratamentoDeClientes.java
@@ -13,14 +13,20 @@ public class TratamentoDeClientes {
         coleccionCliente = new ArrayList<>();
         //Añade al array 3 nuevos clientes, ya que la lista solo permite añadir objetos formato "CLIENTE"
         //el add devuelve true si se añadió o se modificó el tamaño del array a la lista y falso si la lista se mantiene igual y no cambia es decir si no se añade
-        coleccionCliente.add(new Cliente("00000000T","Oliver","3/06/2006"));
-        coleccionCliente.add(new Cliente("12345678Y","Pepe","2/05/2005"));
-        coleccionCliente.add(new Cliente("87654321R","Arturo","1/04/2004"));
+        coleccionCliente.add(new Cliente("00000000T","Oliver","03/06/2006"));
+        coleccionCliente.add(new Cliente("12345678Y","Pepe","02/05/2005"));
+        coleccionCliente.add(new Cliente("87654321R","Arturo","01/04/2004"));
 
         //creamos un nuevo cliente
-        Cliente unCliente = new Cliente("77777777W","Jaime","6/11/2000");
+        Cliente unCliente = new Cliente("77777777W","Jaime","06/11/2000");
         coleccionCliente.add(unCliente);//lo añadimos al arrayList
 
+        //Muestra el total de objetos que tiene la colección
+        System.out.println(coleccionCliente.size());
+        //Muestra si está o no vacía
+        System.out.println(coleccionCliente.isEmpty());
+        //Verifica si contiene un determinado elemento
+        System.out.println(coleccionCliente.contains(unCliente));
         //elimina el cliente creado que fue añadido al arrayList
         coleccionCliente.remove(unCliente);
         //elimina cualquier elemento que tenga el ArrayList
